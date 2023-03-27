f5e9cb59400824d4e4d8e2b33da1ccb343ed8e2e
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2023-03-27 16:57:24 -0500

    Fixed comments, fixed print, reorganized code

--- SENA_PYTHON/ALGORITMOS/retornoInversion.py
+++ SENA_PYTHON/ALGORITMOS/retornoInversion.py
@@ -1,9 +1,11 @@
+#Ejercicio Calcular el ROI
+# formula = "[(ingresos - inversion) / inversion ] x 100"
+
 def investReturn(income, invest):
-    # formula = "[(ingresos - inversion) / inversion ] x 100"
     netIncome = income - invest
     roi = (netIncome / invest) * 100
-    print("La ganancia Neta de la inversión es: ", netIncome, "y el porcentaje del retorno de la inversión es de", roi,"%" )
-    
+    print("La ganancia Neta de la inversión es: ", netIncome)
+    print("y el porcentaje del retorno de la inversión es de", roi,"%" )
 
 income=int(input("Cuanto es la ganancia neta: "))
 invest=int(input("Cuanto fue la inversión: "))
